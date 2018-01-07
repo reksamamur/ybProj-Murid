4d802f31879ce0395656992b139a6575389f8c8f
reksamamur <reksa.kuana9@gmail.com>
2018-01-07 18:17:25 +0700

    Initial commit First

---
+++ app/src/main/java/digyb/la03/ybproj_murid/FillForm.java
@@ -0,0 +1,13 @@
+package digyb.la03.ybproj_murid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FillForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_form);
+    }
+}
