79f2f5c317cc32ffc3430550678c650018be7296
Muhammad Alief <alief.pribadi@gmail.com>
2018-01-09 17:17:45 +0700

    (+)Add Some String

---
+++ app/src/main/java/digyb/la03/ybproj_murid/Config.java
@@ -0,0 +1,31 @@
+package digyb.la03.ybproj_murid;
+
+/**
+ * Created by Quantum Higgs on 1/9/2018.
+ */
+
+public class Config {
+
+    //URL REQUEST
+    public static final String URL_CODE = "https://portly-drainage.000webhostapp.com/isi/code.php";
+    public static final String URL_MURID = "https://portly-drainage.000webhostapp.com/isi/tambah_murid.php";
+
+    //POST REQUEST
+
+
+    // YG DI DALAM STRING SESUAIKAN DENGAN $_POST DI SIDE SERVER
+//    public static final String LOGIN_USER = "user";
+//    public static final String LOGIN_PASS = "pass";
+
+    public static final String MURID_KODE = "kode";
+    public static final String MURID_NAME = "name";
+    public static final String MURID_EMAIL = "email";
+    public static final String MURID_TELP = "telp";
+    public static final String MURID_INFO = "info";
+
+    public static final String CODE_KODE = "kode";
+
+    //STATUS
+    public static final String MURID_ISREG = "success";
+    public static final String CODE_ISLOG = "success";
+}
