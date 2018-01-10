3bfae1fe79cf70b8db0f5d63e3a5a57f7176b92d
reksamamur <reksa.kuana9@gmail.com>
2018-01-10 17:10:02 +0700

    fix susah

--- app/src/main/java/digyb/la03/ybproj_murid/SoalsAdapter.java
+++ app/src/main/java/digyb/la03/ybproj_murid/SoalsAdapter.java
@@ -11,6 +11,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.net.SocketAddress;
 import java.util.ArrayList;
 
 import butterknife.OnClick;
@@ -21,33 +22,23 @@ import butterknife.OnClick;
 
 public class SoalsAdapter extends RecyclerView.Adapter<SoalsAdapter.SoalViewHolder> {
     ArrayList<Soals> soals = new ArrayList<Soals>();
+    int lastchecked = -1;
+    String[] questionsList;
     //Context context;
 
-    public SoalsAdapter(ArrayList<Soals> soals){
+    public SoalsAdapter(ArrayList<Soals> soals, String[] questionsList){
         //this.context = context;
         this.soals = soals;
+        /*
+        soals = new ArrayList<>();
+        for (int i = 0; i < questionsList.length; i++) {
+            soals.add("Not Attempted");
+        }*/
     }
     @Override
     public SoalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card,parent,false);
         SoalViewHolder soalViewHolder = new SoalViewHolder(view);
-
-        /*
-        RadioButton nol = view.findViewById(R.id.jawabA);
-        RadioButton one = view.findViewById(R.id.jawabB);
-        RadioButton two = view.findViewById(R.id.jawabC);
-        RadioButton three = view.findViewById(R.id.jawabD);
-        RadioButton four = view.findViewById(R.id.jawabE);
-        RadioButton five = view.findViewById(R.id.jawabF);
-
-        nol.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                    ;
-            }
-        });*/
-
         return soalViewHolder;
     }
 
@@ -55,6 +46,12 @@ public class SoalsAdapter extends RecyclerView.Adapter<SoalsAdapter.SoalViewHold
     public void onBindViewHolder(SoalViewHolder holder, int position) {
         Soals so = soals.get(position);
         holder.terlihat_soal.setText(so.getTerlihatsoals());
+        holder.nol.setChecked(position == lastchecked);
+        holder.one.setChecked(position == lastchecked);
+        holder.two.setChecked(position == lastchecked);
+        holder.three.setChecked(position == lastchecked);
+        holder.four.setChecked(position == lastchecked);
+        holder.five.setChecked(position == lastchecked);
     }
 
     @Override
@@ -62,12 +59,64 @@ public class SoalsAdapter extends RecyclerView.Adapter<SoalsAdapter.SoalViewHold
         return soals.size();
     }
 
-    public static class SoalViewHolder extends RecyclerView.ViewHolder{
+    public class SoalViewHolder extends RecyclerView.ViewHolder{
         TextView terlihat_soal;
-
+        RadioButton nol;
+        RadioButton one;
+        RadioButton two;
+        RadioButton three;
+        RadioButton four;
+        RadioButton five;
         public SoalViewHolder(View itemView) {
             super(itemView);
             terlihat_soal = itemView.findViewById(R.id.soalTampil);
+            nol = itemView.findViewById(R.id.jawabA);
+            one = itemView.findViewById(R.id.jawabB);
+            two = itemView.findViewById(R.id.jawabC);
+            three = itemView.findViewById(R.id.jawabD);
+            four = itemView.findViewById(R.id.jawabE);
+            five = itemView.findViewById(R.id.jawabF);
+
+            nol.setOnCheckedChangeListener(new RadioButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+                {
+                    if (isChecked){
+                        lastchecked = getAdapterPosition();
+                    }
+                }
+            });
+            two.setOnCheckedChangeListener(new RadioButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+                {
+                    if (isChecked){
+                        lastchecked = getAdapterPosition();
+                    }
+                }
+            });
+            three.setOnCheckedChangeListener(new RadioButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+                {
+                    if (isChecked){
+                        lastchecked = getAdapterPosition();
+                    }
+                }
+            });
+            four.setOnCheckedChangeListener(new RadioButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+                {
+                    if (isChecked){
+                        lastchecked = getAdapterPosition();
+                    }
+                }
+            });
+            five.setOnCheckedChangeListener(new RadioButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+                {
+                    if (isChecked){
+                        lastchecked = getAdapterPosition();
+                    }
+                }
+            });
         }
     }
 }
