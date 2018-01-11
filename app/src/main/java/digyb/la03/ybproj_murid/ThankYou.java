a6e949e88c550d3db7a622016db2799e4ce9e60b
Muhammad Alief <alief.pribadi@gmail.com>
2018-01-11 10:30:56 +0700

    DONE

--- app/src/main/java/digyb/la03/ybproj_murid/ThankYou.java
+++ app/src/main/java/digyb/la03/ybproj_murid/ThankYou.java
@@ -3,16 +3,33 @@ package digyb.la03.ybproj_murid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThankYou extends AppCompatActivity {
+
+    Button ty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thank_you);
+
+        ty = findViewById(R.id.Thanks);
+
+        ty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoMainActivity();
+            }
+        });
     }
 
-    public void Thanks(){
+    public void gotoMainActivity()
+    {
+        Intent intent = new Intent(ThankYou.this, codeCheck.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         finish();
+        startActivity(intent);
     }
 }
